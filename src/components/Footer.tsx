85d3dc6e7cc689ca641e749c3954c180a87a0aa7
Shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2025-04-23 23:49:59 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -105,7 +105,7 @@ const Footer = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                <span className="text-white/70">FitZone Gym, 123 Fitness Avenue<br />Delhi, India 110001</span>
+                <span className="text-white/70">FitZone Gym, 123 Fitness Avenue<br />Mumbai Nallasopara 401209.</span>
               </li>
             </ul>
           </div>
